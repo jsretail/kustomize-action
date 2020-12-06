19eb77c93db487d810782e127409622be2dda50b
Joe Shearn <joseph.shearn@sainsburys.co.uk>
2020-12-06 05:48:14 +0000

    Update cleanYaml.test.ts

--- src/cleanYaml.test.ts
+++ src/cleanYaml.test.ts
@@ -48,7 +48,7 @@ spec:
               protocol: TCP
           env:
             - name: foo
-              value: 'off'
+              value: 'Off'
             - name: should_be_string
               value: true
           resources:
