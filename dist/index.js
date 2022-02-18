f3bdf89fbb9d4fd07b31923dfaf6e57eb35bd871
thunter1000 <action@github.com>
2022-02-18 16:02:28 +0000

    Commiting dist

--- dist/index.js
+++ dist/index.js
@@ -15893,6 +15893,112 @@ module.exports = {
 
 /***/ }),
 
+/***/ 2836:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+// Note: These are not official or complete. I've basically just made them up based on documentation.
+// They are good enough for now but will probably need to be updated/expanded. 
+// Hopefully people start publishing OpenAPI schemas soon.
+
+const { objectMeta } = __webpack_require__(3040);
+const entityRule = {
+  type: "object",
+  properties: {
+    nets: {
+      type: ["array", "null"],
+    },
+    notNets: {
+      type: ["array", "null"],
+    },
+    selector: { type: "string" },
+    notSelector: { type: "string" },
+    namespaceSelector: { type: "string" },
+    ports: {
+      type: ["array", "null"],
+    },
+    notPorts: {
+      type: ["array", "null"],
+    },
+    serviceAccounts: {
+      names: {
+        type: ["array", "null"],
+      },
+      selector: { type: "string" },
+    },
+  },
+};
+const rule = {
+  type: "object",
+  properties: {
+    action: {
+      type: "string",
+    },
+    metadata: { annotations: {} },
+    protocol: {
+      type: "string",
+    },
+    notProtocol: {
+      type: "string",
+    },
+    icmp: {
+      type: "object",
+    },
+    notIcmp: {
+      type: "object",
+    },
+    ipVersion: {
+      type: "integer",
+    },
+    destination: entityRule,
+    source: entityRule,
+  },
+};
+module.exports = {
+  "/globalnetworkpolicy-crd-v1.json": {
+    $schema: "http://json-schema.org/draft-04/schema#",
+    type: "object",
+    properties: {
+      apiVersion: {
+        type: "string",
+      },
+      kind: {
+        type: "string",
+      },
+      metadata: objectMeta,
+      spec: {
+        type: "object",
+        properties: {
+          selector: {
+            type: "string",
+          },
+          order: {
+            type: "number",
+          },
+          serviceAccountSelector: {
+            type: "object",
+          },
+          types: {
+            type: "array",
+            items: [],
+          },
+          ingress: {
+            type: ["array", "null"],
+            items: [rule],
+          },
+          egress: {
+            type: ["array", "null"],
+            items: [rule],
+          },
+        },
+      },
+    },
+    required: ["apiVersion", "kind", "metadata", "spec"],
+  },
+};
+
+
+/***/ }),
+
 /***/ 5225:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -15903,6 +16009,7 @@ const sealedSecrets = __webpack_require__(216);
 const networkPolicy = __webpack_require__(7810);
 const customResourceDefinition = __webpack_require__(5306);
 const istio = __webpack_require__(769);
+const globalnetworkpolicy = __webpack_require__(2836);
 
 const schemas = {
   ...azureIdentity,
@@ -15910,6 +16017,7 @@ const schemas = {
   ...azureIdentityBinding,
   ...sealedSecrets,
   ...networkPolicy,
+  ...globalnetworkpolicy,
   ...customResourceDefinition,
   ...istio,
 };
